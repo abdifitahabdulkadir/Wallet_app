10e886a8f51bcde24cc532c7971f101a74256ac3
Abdifitah Seaph <82603475+abdifitahseaph@users.noreply.github.com>
2023-04-28 17:47:23 +0530

    == Combined Other small changes I made on. (#3)
    * commit all of them
    
    * == Changes on only home_screen and Main.
    
    -- Home screen--
    ->As we talked our last meeting we do need to add addBackButton(<-)
    -> so we can back for previous page while navigatiing.
    
    - for that feature, since also we do have two buttons at back and changed
    --that two buttons into BottomNavigationBar class that gives way to add tappable buttons
    and simply easy for navigations.
    
    -- so i add those into both home icon and setting Icon and we can change letter on
    for icons and what they do for us.
    -- to get back and go into any page I create routes(screen and pages)
    -- which allows me to register any page in main.dart file.
    
    -- Here all of my files have one main MaterialApp() class and that is only
    -- in main class others have only scaffold to control thier screen sizes.
    
    -- Note: the both class those I am calling when I clik the bottonNavigation buttons
    -- have no any scaffold because home_screen manages the entire scren of both buttons
    -- if we need to have our own scaffold we can manage and it is easy.
    -- as default the Class that returns last 6 cards is on first page
    -- and it is icon labbeled with (Home icon) and it is active with green color.
    
    -- main.dart changes
    the main file in the where is MaterialApp() class to manage all our navigations
    - I create three routes and I also give name of your file (it was page2.dart)
    -but now I gave name of import_card.dart(with class ImportCard()).
    
    -- I also add common desgin configuration theme in main.dart
    -- like themes for texts and colos.
    
    -- these navigations it is the section f 22: of our flutter course.
    
    ------------------------------Done-----------------------------------
    
    * == Combined Other small changes I made on.
    
    -- 1.TAKING YOUR FILE INTO NEW PAGE AND THEN RENAME
    =====================================================
    -- I convert the name of page2.dart & take into new file called into
    import_card and it is new screen that pops up when we click the
    addCards button.
    
    -- I also changed your code  how?
    -- previous it was row of three containers with thier design and
    -- child of each container has seperate Icon (camera icon, gallery icon
    -- and search icon).
    -- since all of them have same desing with only the icon and changed into
    what is call builder() - in user defined functions that returns widget
    and takes argument of dynamic value like now I am passing icon then
    it return the same design with any icon you pass.
    
    -- you can look the code.
    
    -- 2. ADDED NEW FONT FOR TITLE.
    ================================================================
     I add 'Roboto" font for title and make the defualt font of app
     using the Theme in the main.dart

--- wallet_app/lib/main.dart
+++ wallet_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 // my custom imports
 import './Screens/home_screen.dart';
+import './Screens/import_card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,40 +11,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
+      initialRoute: "/",
+      routes: {
+        "/": (context) => Home(),
+        ImportCard.routeName: (context) => ImportCard()
+      },
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+          primaryColor: Color.fromRGBO(23, 42, 135, 1),
+          textTheme: TextTheme(
+              bodyLarge: TextStyle(
+            color: Colors.white,
+            fontFamily: "Roboto",
+            fontSize: 30,
+            fontWeight: FontWeight.normal,
+          ))
+      
       ),
-      home: Home(),
+      // home: Home(),
     );
   }
 }
 
 // returning the home bottom design to use again and again
-class HomeButtonDesign extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 400,
-      height: 91,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(16),
-          topRight: Radius.circular(16),
-        ),
-        color: Color.fromRGBO(23, 42, 135, 1),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          Icon(Icons.home_outlined, color: Colors.yellow, size: 50),
-          Icon(Icons.person, color: Colors.grey, size: 50),
-        ],
-      ),
-    );
-  }
-}
+
